8b46bb8b1498a750d8207f6641b49f1ef091a64d
ViniciusRangel <vinirmv23@gmail.com>
2014-05-21 20:41:09 -0300

    subindo classe jokenpoTest

---
+++ jokenpoTest.java
@@ -0,0 +1,19 @@
+import static org.junit.Assert.*;
+
+import java.util.Vector;
+
+import org.junit.Test;
+
+
+public class jokenpoTest {
+
+	@Test
+	public void testDeveReceberUmVetor() {
+		
+		RockPaperScissors rockp = new RockPaperScissors():
+			
+		assertEquals(["Armando", "P"] )
+		
+	}
+
+}
